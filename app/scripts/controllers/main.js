62383d2db36b2b1d5583ec114c42f22a07f93900
Pascal Schrei <pascal.schrei@gmail.com>
2015-11-16 23:12:45 +0100

    Basic desing stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,14 +3,20 @@
 angular.module('kingscupApp')
   .controller('MainCtrl', MainCtrl);
 
-function MainCtrl($mdSidenav, $mdMedia, CardRulesService) {
+function MainCtrl($mdSidenav, $mdMedia, $location, CardRulesService) {
   var vm = this;
   vm.mdMedia = $mdMedia;
   vm.openLeftMenu = openLeftMenu;
+  vm.menuItemClick = menuItemClick;
 
   CardRulesService.initRules();
 
   function openLeftMenu() {
+    $mdSidenav('left').toggle();
+  }
+
+  function menuItemClick(route) {
+    $location.path('/' + route);
     $mdSidenav('left').toggle();
   }
   //todo cookie notification
